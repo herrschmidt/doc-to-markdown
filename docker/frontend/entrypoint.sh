f2fbdc94d9c1d27a023e6b748619ae1657b1eb44
Martin Schmidt <herrschmidt.tv@gmail.com>
2025-01-15 16:43:13 +0100

    Update template files and frontend configuration

--- docker/frontend/entrypoint.sh
+++ docker/frontend/entrypoint.sh
@@ -4,13 +4,8 @@
 set -e
 
 # Create config file with API key
-# Read backend URL from .env file
-MARKDOWN_BACKEND_URL=$(grep MARKDOWN_BACKEND_URL /app/.env | cut -d '=' -f2 | tr -d '\r' | sed -e 's/[[:space:]]*$//')
-
-# Create config file with API key and backend URL
 echo "window.MARKDOWN_CONFIG = {
-  apiKey: '$MARKDOWN_API_KEY',
-  backendUrl: '$MARKDOWN_BACKEND_URL'
+  apiKey: '$MARKDOWN_API_KEY'
 };" > /app/src/config.js
 
 # Start Python HTTP server
--- frontend/src/config.template.js
+++ frontend/src/config.template.js
@@ -1,7 +1,5 @@
 // This file will be overwritten by the entrypoint script
 window.MARKDOWN_CONFIG = {
     apiKey: 'YOUR_API_KEY_HERE', // Replace with your actual API key
-    allowedOrigins: 'http://localhost:8000',
-    rateLimitPerMinute: 60,
-    backendUrl: 'http://localhost:8001'
+    rateLimitPerMinute: 60
 };
